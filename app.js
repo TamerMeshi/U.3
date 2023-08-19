6706dd8a345b9179d1de463b8285bc2e72d1203e
TamerMeshi <td.meshi@gmail.com>
2023-08-19 13:17:07 +0300

    U.3

---
+++ app.js
@@ -0,0 +1,36 @@
+function person (count){
+let pension
+if(count >= 65){
+    console.log( pension ='available')
+} else {
+    console.log(pension = 'Not available')
+}
+
+} 
+person(44)
+
+for(let i = 0; i<101; i++){
+    console.log(i)
+}
+
+let arr1 = []
+for(let i =100 ; i>=0; i--){
+    arr1.push(i)
+    console.log(arr1)
+}
+
+let arr2= []
+
+let i = 10
+while(i >=0){
+    i--
+    console.log(arr2.push(i))
+
+}
+
+for ( i = 0; i < arr2.length-1; i++){
+
+  if ((arr2[i] % 2) === 0){
+  	console.log(arr2[i]);
+  }
+}
